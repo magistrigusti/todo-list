530da2cdcd6bcfb501c052f58b3998d9a96ecf30
Magistry <Magistrigusti@yandex.ru>
2023-02-17 09:37:15 +0200

    first comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist />
     </div>
+  );
+}
+
+function Todolist () {
+  return (
+    <div>
+      <h3>What to learn</h3>
+        <div>
+          <input />
+          <button>+</button>
+        </div>
+        <ul>
+          <li><input type='checkbox' checked={true} /><span>HTML</span></li>
+          <li><input type='checkbox' checked={true} /><span>CSS</span></li>
+          <li><input type='checkbox' checked={true} /><span>JavaScript</span></li>
+          <li><input type='checkbox' checked={false} /><span>react</span></li>
+          <li><input type='checkbox' checked={false} /><span>redux</span></li>
+          <li><input type='checkbox' checked={false} /><span>TypeScript</span></li>
+        </ul>
+        <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+      </div>
   );
 }
 
